66f807ecdb175da4cac7ca331d9eee4d8f808839
avsadmin <adithya.vinayak6@gmail.com>
2023-02-25 18:50:12 +0530

    Dist 1

--- src/constants/constants.ts
+++
@@ -1,4 +0,0 @@
-import { Address } from "ton";
-
-export const mnemonic = "multiply soft federal ladder setup fashion puzzle mask never family item pitch fold element urge fix eagle interest acid stumble column try post tobacco"; // your 24 secret words (replace ... with the rest of the words)
-export const counterAddress = Address.parse("EQCdZK7KtNdfdIJl5aOcz7-TOFldSDCOUZLJKaBYolnAtAeF"); // replace with your address from step 8
